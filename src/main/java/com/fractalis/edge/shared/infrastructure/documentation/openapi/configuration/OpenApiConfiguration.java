dcb2f888b0c6444d7deef4092600a23fb11b6bbd
Lino Abraham Quenta <89022192+linoabraham@users.noreply.github.com>
2024-10-26 18:42:32 -0500

    Update OpenApiConfiguration.java

--- src/main/java/com/fractalis/edge/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/com/fractalis/edge/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -7,10 +7,19 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+package com.fractalis.edge.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class OpenApiConfiguration {
     @Bean
-    public OpenAPI ztechEdgePlatformOpenApi() {
+    public OpenAPI FractilesEdgePlatformOpenApi() {
         var openApi = new OpenAPI();
 
         openApi
@@ -20,7 +29,7 @@ public class OpenApiConfiguration {
                         .version("1.0.0")
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("https://springdoc.org")))
+                                .url("")))
                 .externalDocs(new ExternalDocumentation()
                 .description("Fractalies")
                 .url(""));
